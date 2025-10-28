84cba4ebf5545b9088c29428797b2ca1dd50f3ef
MoeezAhmedKhan <moeezahmedkhan!@gmail.com>
2025-10-27 22:16:24 -0700

    Matplotlib starts

---
+++ PythonLibs/matplotlib/index.py
@@ -0,0 +1,14 @@
+# Matplotlib is used for data visualization
+
+# Problem Statement:
+# Suppose we have monthly sales data for a whole year and we want to find out which month had the highest sales.
+# Manually checking and comparing the numbers would be time-consuming and prone to errors. 
+# By using Matplotlib, we can create a visual representation of the sales data, 
+# making it easy to identify trends and quickly determine the month with the highest sales.
+
+# Data Visualization - The process of representing raw data visually using charts or graphs, 
+# such as bar charts, line charts, etc., to make it easier to understand and analyze.
+
+import matplotlib.pyplot
+
+
